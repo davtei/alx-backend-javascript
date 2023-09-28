80d15f01b42bf0dd2c0bb4108acb740b4185e827
David Tei <daveteidt@gmail.com>
2023-09-28 15:04:04 +0000

    Add classroom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,11 @@
-
+/**
+ * Defines a ClassRoom class.
+ */
 export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom} instance.
+   * @param {Number} maxStudentsSize - max number of students in the classroom
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize
   }
