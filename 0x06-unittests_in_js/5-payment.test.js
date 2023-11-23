cd0389071c0387cfad1923375d100057a701ed3c
David Tei <daveteidt@gmail.com>
2023-11-23 23:23:06 +0000

    Add unit tests for sendPaymentRequestToApi function

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentsToApi', () => {
+  let spy;
+
+  beforeEach(() => {
+    if (!spy) {
+      spy = sinon.spy(console, 'log');
+    }
+  });
+
+  afterEach(() => {
+    spy.log.resetHistory();
+  });
+
+  it('sendPaymentsToApi(100, 20) logs "The total is: 120" to console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.log.calledWith('The total is: 120')).to.be.true;
+    expect(spy.log.calledOnce).to.be.true;
+  });
+
+  it('sendPaymentsToApi(10, 10) logs "The total is: 20" to console', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.log.calledWith('The total is: 20')).to.be.true;
+    expect(spy.log.calledOnce).to.be.true;
+  });
+});
