776fe72507fa623ad6c522942ca1e071f3e8b6c9
David Tei <daveteidt@gmail.com>
2023-11-23 17:22:54 +0000

    Add displayMessage function to print a string

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Prints to stdout a string argument.
+ * @param {string} string - The string to be printed.
+ * @returns {undefined} undefined - This function does not return anything.
+ * @author David Tei <https://github.com/davtei>
+ */
+const displayMessage = (string) => {
+  console.log(string);
+};
+
+module.exports = displayMessage;
