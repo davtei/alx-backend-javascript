ec0b1f416fc4b3c185bf1af627f504be6dae5286
David Tei <daveteidt@gmail.com>
2023-11-24 01:29:07 +0000

    Add API integration tests

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,52 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API Integration Testing', () => {
+  const url = 'http://localhost:7865';
+
+  it('GET / returns the correct response', (done) => {
+    request.get(`${url}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${url}/cart/32`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 32');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns a 404 for negative numbers', (done) => {
+    request.get(`${url}/cart/-1`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns a 404 for non-numeric numbers in :id', (done) => {
+    request.get(`${url}/cart/a1`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('POST /login returns a valid response', (done) => {
+    request.post(`${url}/login`, { form: { userName: 'Betty' } }, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Betty');
+      done();
+    });
+  });
+
+  it('GET /available_payments returns the correct response', (done) => {
+    request.get(`${url}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('{"payment_methods":{"credit_cards":true, "paypal":false}}');
+      done();
+    });
+  });
+});
