83c5a4b3ab8a6e26936f8bc612a48bda1a8737bd
David Tei <daveteidt@gmail.com>
2023-11-24 00:20:13 +0000

    Add API integration tests

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API Integration Testing', () => {
+  const url = 'http://localhost:7865';
+
+  it('GET / returns the correct response', (done) => {
+    request.get(`${url}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${url}/cart/32`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 32');
+      done();
+    });
+  });
+});
